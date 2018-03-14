8406bc3177cc3328f477b2424c44e8629d70c4dc
csk <csk@ableton.com>
2018-03-14 11:44:10 +0100

    Handle error when no completion could be provided in time

--- lldbserver/serviceproxy.py
+++ lldbserver/serviceproxy.py
@@ -39,7 +39,7 @@ class LldbServiceProxy(object):
         with self.completion_condition:
             self.completion_condition.wait_for(
                 lambda: self.completion_result is not None, timeout)
-            matches = list(self.completion_result)
+            matches = list(self.completion_result) if self.completion_result else []
             self.completion_result = None
         print("Completion finished in", time.time() - now, "seconds")
 
